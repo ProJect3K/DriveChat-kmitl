0dd85886f00da2d488b6f5a8ee851a9f6c4270d6
Varaporn Sukmuang <wrp.sukmuang@gmail.com>
2024-10-05 10:16:50 +0700

    Change chat in templete to page

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
