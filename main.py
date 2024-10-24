bec857896bc9e4cc34189692af040bc4fd1ff3cd
67070194 <67070194@kmitl.ac.th>
2024-10-24 18:30:47 +0700

    add-middleware-func-book_1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],  # In production, replace with your frontend URL
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
